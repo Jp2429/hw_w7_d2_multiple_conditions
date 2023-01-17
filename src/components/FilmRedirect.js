1764a4e31823fa92104ea0a0351de51e27c1f86a
John Panton <johnpanton29@outlook.com>
2023-01-17 14:11:31 +0000

    made a bit of code look better

--- src/components/FilmRedirect.js
+++ src/components/FilmRedirect.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 
 const FilmRedirect=()=>{
-
+    const message="View More upcoming releases>>"
     const handleClick=()=>{
         window.location.replace('https://www.imdb.com/calendar/?region=gb');
+        
     }
     return(
-        <button id="redirect-button" onClick={()=>handleClick()}>View More upcoming releases>></button>
+        <button id="redirect-button" onClick={()=>handleClick()}>{message}</button>
     )
 }
 
